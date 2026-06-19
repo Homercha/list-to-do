3f8f98f8ba007dc9a9dc76508962a9abdf087034
Homercha <168032043+Homercha@users.noreply.github.com>
2026-06-19 09:38:12 +0300

    Add files via upload

---
+++ unittestf.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class TestTodo(unittest.TestCase):
+
+    def setUp(self):
+        self.tasks = []
+
+    def test_add_task(self):
+        self.tasks.append("Task1")
+        self.assertEqual(len(self.tasks), 5)
+
+    def test_delete_task(self):
+        self.tasks.append("Task1")
+        self.tasks.pop(0)
+        self.assertEqual(len(self.tasks), 0)
+
+    def test_empty_task(self):
+        self.assertEqual(len(self.tasks), 0)
+
+
+unittest.main(argv=[''], exit=False)
