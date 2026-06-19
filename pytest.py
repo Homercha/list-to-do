e86059179c2d4230aa1e0b66a7ed603b03b7a831
Homercha <168032043+Homercha@users.noreply.github.com>
2026-06-19 10:05:09 +0300

    Add files via upload
    Pytest

---
+++ pytest.py
@@ -0,0 +1,15 @@
+def test_add_task():
+    assert True
+
+
+def test_delete_task():
+    assert True
+
+
+def test_invalid_task():
+    try:
+        [].pop(5)
+    except IndexError:
+        assert True
+
+        
